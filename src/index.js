dee331f185327239eec0b3acbb44c72dcd92a6e2
UnDragon <31067730+UnDragon@users.noreply.github.com>
2018-02-21 11:20:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0,
+        watched = [];
+
+    for (let i = 0; i < preferences.length; i++) {
+        let first = preferences[i],
+            second = preferences[preferences[i] - 1],
+            third = preferences[preferences[preferences[i] - 1] - 1];
+        if (i === preferences[preferences[preferences[i] - 1] - 1] - 1 && 
+            (first !== second || second !== third) && !watched.includes(first) && !watched.includes(second) 
+            && !watched.includes(third)) {
+            count++;
+            watched.push(first, second, third);
+        }
+    }
+
+    return count;
 };
